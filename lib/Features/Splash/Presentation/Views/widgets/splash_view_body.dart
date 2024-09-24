d9d6d055e4af7a43420726928922c2910820c868
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-24 13:54:00 +0300

    change theme & add text to splash view

--- lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -11,6 +12,17 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(logo),
+        const SizedBox(
+          height: 7,
+        ),
+        Text(
+          style: GoogleFonts.kalam(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          "Read Free Books",
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
