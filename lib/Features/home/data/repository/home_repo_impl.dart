7357f0c16cfde11144c049b1a612d881b480d039
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-27 16:18:39 +0300

    implement fetch featured books

--- lib/Features/home/data/repository/home_repo_impl.dart
+++ lib/Features/home/data/repository/home_repo_impl.dart
@@ -33,7 +33,24 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint: 'volumes?q=subject:programming&Filtering=free-ebooks');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left((ServerFailure.fromDioError(e)));
+      }
+
+      return left(ServerFailure(errmessage: e.toString()));
+    }
   }
 }
