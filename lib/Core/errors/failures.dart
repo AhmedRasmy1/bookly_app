07dca2e181eeedb56ebb21767f01d3ded4814c6b
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-27 15:34:32 +0300

    create servier failure from dio error

--- lib/Core/errors/failures.dart
+++ lib/Core/errors/failures.dart
@@ -1,6 +1,50 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errmessage;
+  Failure({required this.errmessage});
+}
 
 class ServerFailure extends Failure {
-  final String? message;
-  ServerFailure(String string, {this.message});
+  ServerFailure({required super.errmessage});
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errmessage: 'Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errmessage: 'Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errmessage: 'Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(errmessage: 'Bad certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromBadResponse(
+          dioError.response!.statusCode!,
+          dioError.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure(errmessage: 'Request to ApiServer was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure(errmessage: 'Connection error with ApiServer');
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure(errmessage: 'No internet connection');
+        }
+        return ServerFailure(errmessage: 'Unexpected error occurred');
+      default:
+        return ServerFailure(errmessage: 'Something went wrong');
+    }
+  }
+  factory ServerFailure.fromBadResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errmessage: response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure(errmessage: 'Status code: 404');
+    } else if (statusCode == 500) {
+      return ServerFailure(errmessage: 'Status code: 500');
+    }
+
+    return ServerFailure(errmessage: 'Something went wrong');
+  }
 }
--- lib/Features/home/data/repository/home_repo_impl.dart
+++ lib/Features/home/data/repository/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Core/utils/api_service.dart';
 import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repository/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -22,8 +23,12 @@ class HomeRepoImpl implements HomeRepo {
       }
 
       return right(books);
-    } on Exception catch (e) {
-      return left(ServerFailure(e.toString()));
+    } catch (e) {
+      if (e is DioException) {
+        return left((ServerFailure.fromDioError(e)));
+      }
+
+      return left(ServerFailure(errmessage: e.toString()));
     }
   }
 
