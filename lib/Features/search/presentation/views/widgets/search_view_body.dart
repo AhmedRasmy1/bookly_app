bcbbce5c442eadb715d2af2bc81484bea20dcf50
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-30 09:15:49 +0300

    display data in search page part 3

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/search/presentation/manager/cubit/serach_books_cubit.dart';
 import 'package:bookly_app/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'custom_search.dart';
 import 'package:flutter/material.dart';
@@ -29,7 +30,26 @@ class SearchViewBody extends StatelessWidget {
             style: TextStyle(fontSize: 20),
           ),
           const SizedBox(height: 10),
-          const Expanded(child: ResultSearchListView()),
+          Expanded(
+            child: BlocBuilder<SerachBooksCubit, SerachBooksState>(
+              builder: (context, state) {
+                if (searchKey.isNotEmpty) {
+                  return const ResultSearchListView();
+                } else {
+                  return Center(
+                    child: Text(
+                      "Still need to search something?",
+                      style: GoogleFonts.agdasima(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
